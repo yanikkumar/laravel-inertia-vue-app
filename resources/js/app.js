0ff0b31932d8278d69e762eb3ca8792a40c3fb4b
Yanik Kumar <yanikkumar@cdac.in>
2025-05-23 11:24:10 +0530

    Added tailwind forms, fontawesome cdn, font and mailtrap setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,7 +12,7 @@ createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
         let page = pages[`./Pages/${name}.vue`];
-        
+
         page.default.layout = page.default.layout || Main;
         return page;
     },
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,13 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
+    {{-- Fontawesome CDN --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    {{-- Google Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+
     @vite('resources/js/app.js')
     @inertiaHead
     @routes
 </head>
 
-<body>
+<body class="font-Montserrat">
     @inertia
 </body>
 
